ad84db5a0f8daa191df9d6374ceec1348e34f398
jayderson gutierrez diaz <jaydergut@hotmail.com>
2020-06-08 16:18:04 -0500

    finish tournament admin

---
+++ Soccer.Web/Models/GroupDetailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Soccer.Web.Data.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Models
+{
+    public class GroupDetailViewModel : GroupDetailEntity
+    {
+        public int GroupId { get; set; }
+
+        [Required(ErrorMessage = "el campo {0} es obligatorio.")]
+        [Display(Name = "Equipo")]
+        [Range(1, int.MaxValue, ErrorMessage = "debe seleccionar un equipo.")]
+        public int TeamId { get; set; }
+
+        public IEnumerable<SelectListItem> Teams { get; set; }
+
+    }
+}
---
+++ Soccer.Web/Models/MatchViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Soccer.Web.Data.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Models
+{
+    public class MatchViewModel : MatchEntity
+    {
+        public int GroupId { get; set; }
+
+        [Required(ErrorMessage = "el campo {0} es obligatorio.")]
+        [Display(Name = "Local")]
+        [Range(1, int.MaxValue, ErrorMessage = "debe seleccionar un equipo.")]
+        public int LocalId { get; set; }
+
+        [Required(ErrorMessage = "el campo {0} es obligatorio.")]
+        [Display(Name = "Visitor")]
+        [Range(1, int.MaxValue, ErrorMessage = "debe seleccionar un equipo.")]
+        public int VisitorId { get; set; }
+
+        public IEnumerable<SelectListItem> Teams { get; set; }
+
+    }
+}
+
