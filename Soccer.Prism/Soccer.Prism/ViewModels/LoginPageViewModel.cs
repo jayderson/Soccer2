f159d560fde82b70feba406d2db8f80012734249
jayderson gutierrez diaz <jaydergut@hotmail.com>
2020-06-22 17:36:37 -0500

    Login App Part 1

--- Soccer.Prism/Soccer.Prism/ViewModels/LoginPageViewModel.cs
+++ Soccer.Prism/Soccer.Prism/ViewModels/LoginPageViewModel.cs
@@ -1,18 +1,63 @@
 ﻿using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace Soccer.Prism.ViewModels
 {
     public class LoginPageViewModel : ViewModelBase
     {
-        public LoginPageViewModel(INavigationService navigationService)
-            :base(navigationService)
+        private bool _isRunning;
+        private bool _isEnabled;
+        private string _password;
+        private DelegateCommand _loginCommand;
+        private DelegateCommand _registerCommand;
+
+        public LoginPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            Title = ("Login");
+            IsEnabled = true;
+        }
+
+        public DelegateCommand LoginCommand => _loginCommand ?? (_loginCommand = new DelegateCommand(LoginAsync));
+
+        public DelegateCommand RegisterCommand => _registerCommand ?? (_registerCommand = new DelegateCommand(RegisterAsync));
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        public string Email { get; set; }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private async void LoginAsync()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+
+                return;
+            }
+        }
+
+        private void RegisterAsync()
+        {
         }
     }
 }
